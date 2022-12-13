87d4b119177f5c5e095eb240dec91bddeeda13d9
Marius <mafloschmid@aol.com>
2022-12-13 22:53:19 +0100

    remove code style issues

--- examples/example.py
+++ examples/example.py
@@ -1,46 +1,29 @@
 class Car:
 
-    ########## Init ##########
-
     def __init__(self, position=0, distance=100, speed=5):
 
         self.speed = speed  # start speed in m/s (1 m/s = 3.6 km/h)
-
         # (individual) reaction time of the driver in seconds
         self.reactiontime = .2
-
         self.length = 5  # meter
-
         self.distance = distance  # distance to the car in front of the driver
-
         self.position = position  # position on the current road
-
         # Individual "confi" speed multiplier. Driver wants to drive speedlimit * speedfactor on a free road
         self.speedfactor = random.random()/10+1
-
         self.acceleration = .5  # Undimensional for know, should become meter/second²
-
         self.speedlimit = 100/3.6  # in meter/second
-
         self.timestepsize = .005  # in seconds
-
         self.speedhistory = []
-
         self.positionhistory = []
-
         self.distancehistory = [self.distance] * \
             int(self.reactiontime/self.timestepsize+1)
 
     def update(self):
 
         self.speedhistory.append(self.speed)
-
         self.positionhistory.append(self.position)
-
         self.distancehistory.append(self.distance)
-
         gradient = self.calculate_gradient_hamiltonian()
-
         self.speed = self.speed + self.timestepsize*gradient*self.acceleration*2 + \
             (random.random() - 0.5) / \
             10  # Mulitplikator entspricht der Beschleunigung
@@ -64,12 +47,10 @@ class Car:
     def speed_hamiltonian(self, speed):
 
         if speed > self.speedfactor * self.speedlimit:
-
             hamiltonian = (speed - self.speedfactor *
                            self.speedlimit)**4 / self.speedlimit
 
         else:
-
             hamiltonian = (speed - self.speedfactor *
                            self.speedlimit)**2 / self.speedlimit
 
